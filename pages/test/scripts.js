ee1fc3acc1b20223083aaee6325d062abfb2ac37
herman <hermanjustino@gmail.com>
2023-09-13 21:15:46 -0400

    Updated navbar and footer

---
+++ pages/test/scripts.js
@@ -0,0 +1,5 @@
+function toggleMobileMenu() {
+    const mobileDropdown = document.getElementById("mobileDropdown");
+    mobileDropdown.style.display = mobileDropdown.style.display === "block" ? "none" : "block";
+  }
+  
