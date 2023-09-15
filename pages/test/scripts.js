a9f609419a56fe67bca845ca530579b4db2a3641
herman <hermanjustino@gmail.com>
2023-09-15 14:10:46 -0400

    Typewriter on about page

--- pages/test/scripts.js
+++ pages/test/scripts.js
@@ -2,4 +2,24 @@ function toggleMobileMenu() {
     const mobileDropdown = document.getElementById("mobileDropdown");
     mobileDropdown.style.display = mobileDropdown.style.display === "block" ? "none" : "block";
   }
+  const text = `This is a typewriter-style effect.
+  It gradually fills the paragraph with multiple lines of text.
+  The textbox expands as the paragraph grows.
+  It works on both desktop and mobile.`;
+  
+  const typewriterText = document.getElementById("typewriter-text");
+  let charIndex = 0;
+  
+  function typeWriter() {
+      if (charIndex < text.length) {
+          typewriterText.innerHTML += text.charAt(charIndex);
+          charIndex++;
+          setTimeout(typeWriter, 50); // Adjust the typing speed here (milliseconds)
+      }
+  }
+  
+  // Start the typewriter effect when the page loads
+  window.onload = function() {
+      typeWriter();
+  };
   
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,3 +7,23 @@ function navbarDropdown() {
     x.className = "topnav";
   }
 }
+
+const text = `Outlet Network is both a platform and a community 
+that brings together Toronto creatives in an attempt to bridge 
+the gap between artists and Toronto’s creative community.`;
+
+const typewriterText = document.getElementById("typewriter-text");
+let charIndex = 0;
+
+function typeWriter() {
+    if (charIndex < text.length) {
+        typewriterText.innerHTML += text.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeWriter, 40); // Adjust the typing speed here (milliseconds)
+    }
+}
+
+// Start the typewriter effect when the page loads
+window.onload = function() {
+    typeWriter();
+};
