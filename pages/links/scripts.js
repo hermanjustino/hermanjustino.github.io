12a579d895be369306477e65cd41525671e13fc4
herman <hermanjustino@gmail.com>
2023-12-01 21:46:55 -0500

    Updated links page to have animated list and background

--- pages/links/scripts.js
+++ pages/links/scripts.js
@@ -3,3 +3,21 @@ function share(url) {
     alert(`Share ${url}`);
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    const linkList = document.querySelector('.link-list');
+    
+    // Add 'visible' class after a delay
+    setTimeout(function() {
+      linkList.classList.add('visible');
+    }, 500); // Adjust the delay as needed
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const background = document.getElementById('background');
+    background.style.animation = 'none'; // Disable animation initially
+    setTimeout(() => {
+      background.style.animation = ''; // Enable animation after a brief delay
+    }, 500);
+  });
+  
+  
